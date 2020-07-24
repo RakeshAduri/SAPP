3ebc432f3b988b16b6ef6224f12a92194d7dd747
RakeshAduri <60639774+RakeshAduri@users.noreply.github.com>
2020-07-24 10:35:23 +0530

    Add files via upload

---
+++ simpleInterest1/src/main/java/simpleInterest1/simpleInterest.java
@@ -0,0 +1,14 @@
+package simpleInterest1;
+
+
+public class simpleInterest {
+
+		public static double simpleinterest(double principleamount, double time, double rate) {
+			double si = (principleamount * time * rate)/100;
+			return si;
+		}
+		
+	}
+
+
+
