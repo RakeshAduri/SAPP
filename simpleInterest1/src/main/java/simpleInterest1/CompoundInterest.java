187f1593a84678898dc642ba9e3d2db91d14eef4
RakeshAduri <60639774+RakeshAduri@users.noreply.github.com>
2021-08-26 18:31:34 +0530

    Delete simpleInterest1 directory

--- simpleInterest1/src/main/java/simpleInterest1/CompoundInterest.java
+++
@@ -1,9 +0,0 @@
-package simpleInterest1;
-
-public class CompoundInterest {
-
-	public double compoundinterest(double principleamount, double rate, double time) {
-		double ci = principleamount * Math.pow(1.0+rate/100.0,time) - principleamount;
-		return ci;
-	}
-}
